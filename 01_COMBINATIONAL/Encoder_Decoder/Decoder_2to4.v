55c48880f911677b47d5b153831e2b725c845dd1
Keshav Padia <keshavpadia48@gmail.com>
2026-07-12 20:44:51 +0530

    Update Decoder_2to4.v

--- 01_COMBINATIONAL/Encoder_Decoder/Decoder_2to4.v
+++ 01_COMBINATIONAL/Encoder_Decoder/Decoder_2to4.v
@@ -1,1 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.07.2026 19:41:18
+// Design Name: 
+// Module Name: Decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decoder(sel,out);
+input[1:0] sel;
+output reg[3:0] out;
+always @(*)
+begin
+case(sel)
+2'b00: out = 4'b0001;
+2'b01: out = 4'b0010;
+2'b10: out = 4'b0100;
+2'b11: out = 4'b1000;
+endcase
+end
+endmodule
 
