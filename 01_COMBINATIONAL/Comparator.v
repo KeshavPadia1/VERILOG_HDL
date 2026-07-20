a3d033a28c790469075f5889b1b7cce5dec6716a
Keshav Padia <keshavpadia48@gmail.com>
2026-07-20 23:58:00 +0530

    Add files via upload

---
+++ 01_COMBINATIONAL/Comparator.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.07.2026 23:43:32
+// Design Name: 
+// Module Name: Comparator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Comparator(
+
+    );
+endmodule
