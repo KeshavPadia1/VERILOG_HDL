cf766ab01b55a4c50c256f43cb7cbe499a9a3cfa
Keshav Padia <keshavpadia48@gmail.com>
2026-07-22 20:07:48 +0530

    Update MUL_tb.v

--- DATAPATH_CONTROLLER/MUL_tb.v
+++ DATAPATH_CONTROLLER/MUL_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.07.2026 02:13:41
-// Design Name: 
-// Module Name: MUL_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module MUL_tb;
 
