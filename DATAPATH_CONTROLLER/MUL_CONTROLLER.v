fdf9404edfd55044440625ced6741d6b076976e2
Keshav Padia <keshavpadia48@gmail.com>
2026-07-22 20:07:34 +0530

    Update MUL_CONTROLLER.v

--- DATAPATH_CONTROLLER/MUL_CONTROLLER.v
+++ DATAPATH_CONTROLLER/MUL_CONTROLLER.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.07.2026 01:47:47
-// Design Name: 
-// Module Name: MUL_CONTROLLER
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module MUL_CONTROLLER(Clk,RST,Done,EqZ,START,LdA,LdB,LdP,ClrP,DecB);
 
