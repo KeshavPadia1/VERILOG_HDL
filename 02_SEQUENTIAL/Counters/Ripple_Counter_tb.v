9d80125c6f0557359f5bf98c6e84b9bf213d9c26
Keshav Padia <keshavpadia48@gmail.com>
2026-07-24 22:08:35 +0530

    Update Ripple_Counter_tb.v

--- 02_SEQUENTIAL/Counters/Ripple_Counter_tb.v
+++ 02_SEQUENTIAL/Counters/Ripple_Counter_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.07.2026 14:23:34
-// Design Name: 
-// Module Name: Ripple_Counter_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Ripple_Counter_tb;
 reg clk, clr;
