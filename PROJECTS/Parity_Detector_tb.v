3467125950d097bf15ad45052a2fe4ea58e8c5e4
Keshav Padia <keshavpadia48@gmail.com>
2026-07-28 23:45:28 +0530

    Update Parity_Detector_tb.v

--- PROJECTS/Parity_Detector_tb.v
+++ PROJECTS/Parity_Detector_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.07.2026 13:49:24
-// Design Name: 
-// Module Name: Parity_Detector_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Parity_Detector_tb;
