ed4f235494e5e80db5580c2d4a0859b2c58fa2fd
Keshav Padia <keshavpadia48@gmail.com>
2026-07-30 15:58:23 +0530

    Update BOOTHS_CONTROLLER.v

--- DATAPATH_CONTROLLER/BOOTHS_CONTROLLER.v
+++ DATAPATH_CONTROLLER/BOOTHS_CONTROLLER.v
@@ -13,20 +13,18 @@ begin
 case(STATE)
 S0: if(Start) STATE<=S1;
 S1: STATE<=S2;
-S2: begin
+S2: #2 begin
     if({Q0,Qm1} == 2'b01) STATE<=S3;
     else if({Q0,Qm1} == 2'b10) STATE<=S4;
-    else if({Q0,Qm1} == 2'b00 || {Q0,Qm1} == 2'b11) STATE<=S5;
+    else  STATE<=S5;
     end
 S3: STATE<=S5;
-S4: STATE<=S4;
-S5: begin
+S4: STATE<=S5;
+S5: #2 begin
     if(EqZ) STATE<=S6;
-    else begin
-    if({Q0,Qm1} == 2'b01) STATE<=S3;
+    else if({Q0,Qm1} == 2'b01) STATE<=S3;
     else if({Q0,Qm1} == 2'b10) STATE<=S4;
-    else if({Q0,Qm1} == 2'b00 || {Q0,Qm1} == 2'b11) STATE<=S5; 
-    end
+    else STATE<=S5;
     end
 S6: STATE<=S6;
 default: STATE<=S0;
@@ -35,13 +33,15 @@ end
 
 always @(STATE)
 begin
+LdA=0; LdQ=0; LdM=0; ClrA=0; ClrQ=0; SftA=0; SftQ=0;
+Clrff=0; LdCnt=0; Dec=0; Done=0; SubAdd=0; 
 case(STATE)
 S0: begin LdA=0;LdQ=0;LdM=0;ClrA=0;ClrQ=0;SftA=0;SftQ=0;Clrff=0;LdCnt=0;Dec=0;Done=0; end
-S1: begin LdM=1;ClrA=1;Clrff=1;LdCnt=1; end
-S2: begin LdM=0;ClrA=0;Clrff=0;LdCnt=0;LdQ=1; end
-S3: begin LdQ=0;SubAdd=0;LdA=1; end
-S4: begin SubAdd=1;LdA=1; end 
-S5: begin Dec=1;SubAdd=0;LdA=0;SftA=1;SftQ=1; end
+S1: begin LdM=1;ClrA=1;Clrff=1;LdCnt=1;LdQ=1; end
+S2: begin LdM=0;ClrA=0;Clrff=0;LdCnt=0; end
+S3: begin LdQ=0;SubAdd=0;LdA=1;end
+S4: begin SubAdd=1;LdA=1;LdQ=0;end 
+S5: begin Dec=1;SubAdd=0;LdA=0;SftA=1;SftQ=1;LdQ=0; end
 S6: begin Done=1; end
 default: begin LdA=0;LdQ=0;LdM=0;ClrA=0;ClrQ=0;SftA=0;SftQ=0;Clrff=0;LdCnt=0;Dec=0;Done=0; end 
 endcase
