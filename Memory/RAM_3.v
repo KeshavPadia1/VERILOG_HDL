c81ff9df7c79ea5ef4262f3c12298711c3877087
Keshav Padia <keshavpadia48@gmail.com>
2026-08-01 22:44:03 +0530

    Update RAM_3.v

--- Memory/RAM_3.v
+++ Memory/RAM_3.v
@@ -1,17 +1,16 @@
-module ram_3 (data_out, data_in, addr, wr, cs);
+`timescale 1ns / 1ps
 
-  parameter addr_size = 10, word_size = 8,
-            memory_size = 1024;
+module RAM_3(data_in,data_out,addr,cs,wr);
 
-  input  [addr_size-1:0]  addr;
-  input  [word_size-1:0]  data_in;
-  input  wr, cs;
-  output [word_size-1:0]  data_out;
-  reg    [word_size-1:0]  mem [memory_size-1:0];
+parameter wordsize = 8, addr_size=10, memory_size=1024;
+input[addr_size-1:0] addr; 
+input[wordsize-1:0] data_in;
+output[wordsize-1:0] data_out;
+reg[wordsize-1:0] memory[memory_size-1:0];
+input cs,wr;
 
-  assign data_out = mem[addr];
+assign data_out = memory[addr];
 
-  always @(wr or cs)
-    if (wr)  mem[addr] = data_in;
-
+always @(wr or cs)
+if(wr) memory[addr] = data_in;
 endmodule
