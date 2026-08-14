6147b09b54998547730ab69ae63be03d69f4438c
Keshav Padia <keshavpadia48@gmail.com>
2026-08-14 23:40:43 +0530

    Add files via upload

---
+++ MIPS32_PROCESSOR/MIPS32_PROCESSOR.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.08.2026 19:44:41
+// Design Name: 
+// Module Name: MIPS32_PROCESSOR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MIPS32_PROCESSOR(
+
+    );
+endmodule
